b2d760b2bf66f72d61fce1f74edb4e972e8e0444
dafei <dafei@ibantang.com>
2020-04-29 10:29:07 +0800

    doc:

--- src/test/java/com/wtgroup/sugar/db/TreeTableHandlerTest.java
+++ src/test/java/com/wtgroup/sugar/db/TreeTableHandlerTest.java
@@ -1,6 +1,7 @@
 package com.wtgroup.sugar.db;
 
 import com.alibaba.fastjson.JSON;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 import java.util.*;
@@ -10,20 +11,13 @@ public class TreeTableHandlerTest {
     @Test
     public void toTree() {
         // 模拟数据
-        List<Map> data = new ArrayList<>();
-        for ( int i = 0; i < 10; i++ ) {
-            data.add(buildRow(i));
-        }
-        HashMap<String, Object> r10 = buildRow(10);
-        r10.put("pid", 3);
-        HashMap<String, Object> r11 = buildRow(11);
-        r11.put("pid", 3);
-        data.add(r10);
-        data.add(r11);
-
-        System.out.println(data);
+        List<Map> data = mock();
 
         // to tree
+        // p1: DB查询到原始数据集
+        // p2: 指定 id 获取方式
+        // p3: pid 获取方式
+        // p4: 原始数据行向java树节点的转换方式
         TreeTableHandler<Map, Map, Integer> treeTableHandler = new TreeTableHandler<>(data,
                 row -> (Integer) row.get("id"),
                 row -> (Integer) row.get("pid"),
@@ -39,6 +33,7 @@ public class TreeTableHandlerTest {
                         return res;
                     }
 
+                    // 当子集数据就绪时调用
                     @Override
                     public void onChildren( Map parent, List<Map> children ) {
                         parent.put("children", children);
@@ -52,11 +47,29 @@ public class TreeTableHandlerTest {
                 }
         );
 
+        // 转换成树
         List<Map> results = treeTableHandler.toTree();
-        System.out.println(treeTableHandler.getTreeMeta());
+        // System.out.println(treeTableHandler.getTreeMeta());
         System.out.println(JSON.toJSONString(results, true));
 
 
+    }
+
+    @NotNull
+    private List<Map> mock() {
+        List<Map> data = new ArrayList<>();
+        for ( int i = 0; i < 10; i++ ) {
+            data.add(buildRow(i));
+        }
+        HashMap<String, Object> r10 = buildRow(10);
+        r10.put("pid", 3);
+        HashMap<String, Object> r11 = buildRow(11);
+        r11.put("pid", 3);
+        data.add(r10);
+        data.add(r11);
+
+        System.out.println(data);
+        return data;
     }
 
     private HashMap<String, Object> buildRow( int i) {
